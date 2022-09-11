2113a4fafa9520a8e648c33efcaf434822ca4cc2
AbdelrahmanHs86 <bodi25sameh@gmail.com>
2022-09-11 22:01:41 +0300

    feat: Add getBooks async thunk

--- src/store/reducers/bookSlice.js
+++ src/store/reducers/bookSlice.js
@@ -1,11 +1,37 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 
+export const getBooks = createAsyncThunk('book/getBooks', async (arg, thunkAPI) => {
+
+    const { rejectWithValue, getState, dispatch } = thunkAPI;
+
+    try {
+        const res = await fetch(' http://localhost:3005/book');
+        const data = await res.json();
+        return data;
+    } catch (e) {
+        rejectWithValue(e.message)
+    }
+
+})
 
 const bookSlice = createSlice({
     name: 'book',
     initialState: { bookslist: [] },
     reducers: {},
+    extraReducers: {
+        //getbooks
+        [getBooks.pending]: (state, action) => {
+
+        },
+        [getBooks.fulfilled]: (state, action) => {
+            state.bookslist = action.payload;
+        },
+        [getBooks.rejected]: (state, action) => {
+            console.log('rejected', action.payload);
+        },
+
+    }
 
 })
 

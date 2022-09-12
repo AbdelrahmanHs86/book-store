0e3adb3fb65ad17cefd999f2a187e6cacced1ce5
AbdelrahmanHs86 <bodi25sameh@gmail.com>
2022-09-12 06:17:45 +0300

    feat: Add book addition main functionality

--- src/Components/AddForm.js
+++ src/Components/AddForm.js
@@ -1,29 +1,65 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { addBooks } from "../store/reducers/bookSlice";
+import { useDispatch } from "react-redux";
 
 const Addform = () => {
+  const dispatch = useDispatch();
+
+  const titleRef = useRef();
+  const priceRef = useRef();
+  const descRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = titleRef.current.value;
+    const price = priceRef.current.value;
+    const desc = descRef.current.value;
+    const bookData = { title, price, desc };
+    dispatch(addBooks(bookData));
+  };
+
   return (
-    <div className='row'>
-      <div className='col-6 offset-3 mt-3'>
+    <div className="row">
+      <div className="col-6 offset-3 mt-3">
         <h2>Insert Book</h2>
         <form>
-          <div className='form-group'>
-            <label htmlFor='title'>Title</label>
-            <input type='text' className='form-control' id='title' required />
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input
+              ref={titleRef}
+              type="text"
+              className="form-control"
+              id="title"
+              required
+            />
           </div>
-          <div className='form-group'>
-            <label htmlFor='price'>Price</label>
-            <input type='number' className='form-control' id='price' required />
+          <div className="form-group">
+            <label htmlFor="price">Price</label>
+            <input
+              ref={priceRef}
+              type="number"
+              className="form-control"
+              id="price"
+              required
+            />
           </div>
-          <div className='form-group'>
-            <label htmlFor='Description'>Description</label>
+          <div className="form-group">
+            <label htmlFor="Description">
+              Description
+            </label>
             <textarea
-              className='form-control'
-              id='Description'
-              rows='3'
+              ref={descRef}
+              className="form-control"
+              id="Description"
+              rows="3"
               required
             ></textarea>
           </div>
-          <button type='submit' className='btn btn-primary'>
+          <button
+            type="submit"
+            onClick={(e) => handleSubmit(e)}
+            className="btn btn-primary"
+          >
             Submit
           </button>
         </form>
